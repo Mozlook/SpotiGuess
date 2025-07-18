23f4ce2bfe4c22e927e12a66c40781a838c08108
Mikolaj Mozoluk <mikolaj.moozoluk@gmail.com>
2025-07-19 01:26:43 +0200

    deleted FetchRecommendations function

--- backend/internal/spotify/client.go
+++ backend/internal/spotify/client.go
@@ -3,7 +3,6 @@ package spotify
 import (
 	"backend/internal/model"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -88,55 +87,4 @@ func FetchRecentTracks(token string) ([]model.Track, error) {
 		})
 	}
 	return tracks, nil
-}
-
-// FetchRecommendations retrieves a list of recommended track titles from the Spotify API
-// based on a given seed track ID.
-//
-// It performs a GET request to the Spotify endpoint:
-//
-//	https://api.spotify.com/v1/recommendations
-//
-// using the provided `trackID` as the seed parameter. The function requests
-// 3 recommendations for the Polish market (market=PL).
-//
-// The response is parsed and the names of the recommended tracks are returned
-// as a slice of strings, which can be used as fake quiz answers or distractors.
-//
-// Parameters:
-//   - trackID: Spotify track ID to use as the recommendation seed
-//   - token: a valid Spotify OAuth access token
-//
-// Returns:
-//   - []string: list of track titles recommended by Spotify
-//   - error: if the request or decoding fails
-func FetchRecommendations(trackID string, token string) ([]string, error) {
-	url := fmt.Sprintf("https://api.spotify.com/v1/recommendations?seed_tracks=%s&limit=3&market=PL", trackID)
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Authorization", "Bearer "+token)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Spotify FetchRecommendations error:", err)
-		return nil, err
-	}
-	if resp.StatusCode != 200 {
-		body, _ := io.ReadAll(resp.Body)
-		log.Printf("Spotify API returned %d: %s", resp.StatusCode, string(body))
-		return nil, fmt.Errorf("spotify error %d", resp.StatusCode)
-	}
-
-	var apiResp recommendationResponse
-
-	defer resp.Body.Close()
-	err = json.NewDecoder(resp.Body).Decode(&apiResp)
-	if err != nil {
-		return nil, err
-	}
-	var fakeAnswers []string
-	for _, track := range apiResp.Tracks {
-		fakeAnswers = append(fakeAnswers, track.Name)
-	}
-
-	return fakeAnswers, nil
 }
